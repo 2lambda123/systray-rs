5d69d63d593d9c07827c10a90f6e379c0c8bb41a
Daniel Abramov <dabramov@snapview.de>
2017-03-01 16:29:43 +0100

    Update `examples` directory

--- examples/trayicon.rs
+++ examples/trayicon.rs
@@ -29,5 +29,14 @@ fn main() {
 
 #[cfg(not(target_os = "windows"))]
 fn main() {
-    panic!("Not implemented on this platform!");
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(e) => panic!("Can't create tray icon app!")
+    }
+
+    const ICON_BUFFER: &'static [u8] = include_bytes!("rust-logo.png");
+    let mut w = &mut app.window;
+    w.set_icon_from_buffer(ICON_BUFFER, 256, 256).unwrap();
+    w.wait_for_message();
 }
